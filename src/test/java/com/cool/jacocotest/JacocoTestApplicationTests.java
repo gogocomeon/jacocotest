fecfa2c1ec29116bfc0917e55085adacbd7a8142
刘晓欣 <liuxiaoxin@51xf.cn>
2021-07-05 17:31:36 +0800

    添加单测

--- src/test/java/com/cool/jacocotest/JacocoTestApplicationTests.java
+++ src/test/java/com/cool/jacocotest/JacocoTestApplicationTests.java
@@ -1,13 +1,27 @@
 package com.cool.jacocotest;
 
+import com.cool.jacocotest.service.AddService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
 @SpringBootTest
 class JacocoTestApplicationTests {
+    @Resource
+    private AddService addService;
 
     @Test
+    void testJacoco(){
+        System.out.println(addService.addTwo(1,2));
+    }
+    @Test
     void contextLoads() {
+    }
+    @Test
+
+    void testXiaoxin(){
+        System.out.println("hello world!");
     }
 
 }
