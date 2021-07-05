af0e1ca37c2144d3a872feb24137cb0ea7aee2b3
刘晓欣 <liuxiaoxin@51xf.cn>
2021-07-05 17:04:41 +0800

    junit修改

--- src/test/java/com/cool/jacocotest/TestService.java
+++ src/test/java/com/cool/jacocotest/TestService.java
@@ -1,7 +1,7 @@
 package com.cool.jacocotest;
 
 import com.cool.jacocotest.service.AddService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.runner.RunWith;
